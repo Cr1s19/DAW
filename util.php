f8410c7d2f351c4512088204250fadb321f2308f
Cr1s19 <Cr1s@System-Cr1s.local>
2015-03-10 14:13:17 -0600

    changes to util

--- util.php
+++ util.php
@@ -1,2 +1,17 @@
-#
+<?php 
+    $mysql=mysqli_connect( "localhost", "root", "", "LAB11");
+    if (!$mysql) {
+    die("Connection failed: ".mysqli_connect_error());
+    }
+#echo "Connect Successful"?>
 
+<?php
+    $query = 'INSERT INTO ANIMES(Nombre, Descripcion) VALUES(?,?)';
+        if (mysqli_query($mysql, $query)) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        }
+?>
+
+<?php mysqli_close($mysql);?>
